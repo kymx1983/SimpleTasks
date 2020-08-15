dd3882949489e700d8b478e1107f8cf9ae7ccfab
kymx1983 <kymx1983@gmail.com>
2020-08-15 21:38:24 +0900

    tasksコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'task/index'
+  get 'task/show'
+  get 'task/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
