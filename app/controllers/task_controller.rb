ee12dc081e810ec2863cbbf6429aec635314a53f
kymx1983 <kymx1983@gmail.com>
2020-08-15 22:12:18 +0900

    登録機能を行う

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -6,6 +6,20 @@ class TaskController < ApplicationController
   end
 
   def new
-    @Task = Task.new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+    @task.title = params[:title]
+    @task.content = params[:content]
+    @task.limit_date = params[:limit_date]
+    @task.status = 0
+
+    if @task.save
+      redirect_to('/task/index')
+    else
+      render("task/new")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'task/index'
   get 'task/show'
   get 'task/new'
+  post "task/create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
