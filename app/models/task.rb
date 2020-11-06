be4f365ec34c8634dd425389bcb8aaafe1fc4e37
kymx1983 <kymx1983@gmail.com>
2020-11-06 22:36:03 +0900

    レイアウト調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,15 @@ class Task < ApplicationRecord
   validates :title, {
       presence: true
   }
+
+  def get_status
+    case status
+    when 0 then
+      "未完了"
+    when 1 then
+      "完了"
+    else
+      "その他"
+    end
+  end
 end
